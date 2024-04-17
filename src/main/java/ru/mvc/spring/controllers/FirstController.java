7bce98dde163c911c7532f1bf6633c9002e4b641
medasumumbs <semen.muravin@gmail.com>
2024-04-17 21:47:00 +0300

    required false added

--- src/main/java/ru/mvc/spring/controllers/FirstController.java
+++ src/main/java/ru/mvc/spring/controllers/FirstController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value ="surname", required = false) String surname) {
         System.out.println("hello, " + name + " "+ surname);
 
         return "first/hello";
