c0d98f8d42fe803e13f1c43075a0ab04603a877c
medasumumbs <semen.muravin@gmail.com>
2024-04-17 22:03:48 +0300

    thymeleaf shows text, model added

--- src/main/java/ru/mvc/spring/controllers/FirstController.java
+++ src/main/java/ru/mvc/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.mvc.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,13 +12,15 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value ="surname", required = false) String surname) {
-        System.out.println("hello, " + name + " "+ surname);
-
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message","hello, " + name + " " + surname);
         return "first/hello";
     }
+
     @GetMapping("/goodbye")
     public String goodByePage() {
-        return  "first/goodbye";
+        return "first/goodbye";
     }
 }
